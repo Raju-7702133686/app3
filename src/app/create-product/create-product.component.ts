c3399b2eecc16ed86bf88026378da35df4100ea5
Raju-7702133686 <Raju-rajuis819@gmail.com>
2023-07-19 14:34:46 +0530

    nested fromgroup created

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -11,7 +11,12 @@ export class CreateProductComponent {
   public productForm: FormGroup = new FormGroup({
     name: new FormControl(),
     price: new FormControl(),
-    color: new FormControl()
+    color: new FormControl(),
+    features: new FormGroup({
+      ram: new FormControl(),
+      battery: new FormControl(),
+      processor: new FormControl()
+    })
   });
 
   submit(){
