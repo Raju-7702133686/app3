0959b6673eda133f28b8cf5913d815534f1adbcb
Raju-7702133686 <Raju-rajuis819@gmail.com>
2023-07-28 13:10:14 +0530

    pre loading completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StructuraldirectoriesComponent } from './structuraldirectories/structuraldirectories.component';
@@ -114,7 +114,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
